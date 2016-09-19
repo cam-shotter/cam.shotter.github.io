8f27f86cdddcb07dc42e4903c7759aaea7dd44e5
cam-shotter <cameronshotter@gmail.com>
2016-09-19 18:01:04 +1200

    put initial content in app.js and index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './client/components/app'
+
+render(<App name="Cam's Personal Webpage" />, document.querySelector('main'))
+console.log('welcome to cams thing')
